cd164649c45f8a7da810fae46ef2ae4a2d66d193
uriel <velasquez_uriel@hotmail.com>
2019-03-13 12:47:05 -0500

    android

---
+++ platforms/android/assets/www/js/usuarioController.js
@@ -0,0 +1,229 @@
+var jUser = function () {
+  return {
+
+      Init : function(){
+
+        var $this = this;
+        this.user                = $("#lgnUser"); 
+        this.password            = $("#lgnPassword");
+        this.btn_login           = $("#btnLogin");
+
+      }, 
+      click_init: function () {
+        var $this = this;
+
+        $("#search_user").keyup(function() {
+          $this.search_user("list_user", $("#search_user").val());
+        });
+
+      },
+      listProyectUser: function(DomElement,project){
+
+        var $this = this;
+        var token = window.localStorage.user_token;
+
+        $$.ajax({
+            url     : 'http://35.211.157.80/appmanager/api/projectmember/index',
+            method  : 'POST',
+            dataType: 'json',
+            headers : {"Authorization": "Bearer " + token,
+                       "Accept"       : "application/json ",
+                       "Content-Type" : "application/x-www-form-urlencoded",},
+            data    :{
+                       'NUM_PROYECTO'        : project,
+                     },
+            success : function(response){
+              var html = "";
+              if(response.data.members.length > 0){
+                  $.each(response.data.members, function( index, value ) {
+
+                    html+= '<li id="userList_'+value.id+'" class="list-group-item">';
+                    html+= '        <a href="javascript:void(0);" class="media">';
+                    html+= '            <div class="media-body">';
+                    html+= '                <h5>'+value.name+'</h5>';
+                    html+= '                <p>'+value.alias+'</p>';
+                    html+= '            </div>';
+                    html+= '            <div class="w-auto"> '; 
+                    html+= '            <small class="text-primary effort-time" onclick="jUser.userOption('+value.id+','+project+')"><i class="f7-icons">more_vertical_fill</i></small>';               
+                    html+= '            </div>';
+                    html+= '        </a>';
+                    html+= '</li>';
+
+
+
+
+                  });
+
+                  $('#'+DomElement).html(html);
+
+              }else{
+                  $('#'+DomElement).html('<p>Sin usuarios</p>');
+              }
+             
+            },
+            error: function(xhr, status){
+              //alert('Error: '+JSON.stringify(xhr));
+              //alert('ErrorStatus: '+JSON.stringify(status));
+            }
+          });
+
+
+        
+
+      },
+      deleteUser: function(userId,proyectoId){
+
+        var $this = this;
+        var token = window.localStorage.user_token;
+        
+        $$.ajax({
+            url     : 'http://35.211.157.80/appmanager/api/projectmember/destroy',
+            method  : 'POST',
+            dataType: 'json',
+            headers  : {"Authorization" : "Bearer " + token,
+                        "Accept"        : "application/json ",
+                        "Content-Type"  : "application/x-www-form-urlencoded",},
+            data:{
+                  'USER_PROYECTO': proyectoId,
+                  'USER_USER':userId
+                  },
+            success: function(response){
+                   if(response.success == true){
+                      //myApp.alert(response.data.message,'Corecto');
+                     jUser.listProyectUser('listUserProject',proyectoId);
+                   }else{
+                      myApp.alert(response.data.message,'Error');
+                   }
+           
+            },
+            error: function(xhr, status){
+            
+              //alert('Error: '+JSON.stringify(xhr));
+              //alert('ErrorStatus: '+JSON.stringify(status));
+            }
+          });
+
+      },
+      userOption:function(userId,proyectoId){
+          var $this   = this;
+          var buttons = [
+                {
+                    text: 'Eliminar del proyecto',
+                    bold: true,
+                    onClick: function () {
+                           myApp.modal({
+                                title:  '¿Deseas eliminar este usuario del proyecto?',
+                                text:   'El usuario no tendra acceso al proyecto despues de ser eliminado',
+                                buttons: [
+                                  {
+                                    text: 'Cancelar'
+                                  },
+                                  {
+                                    text: 'Eliminar',
+                                    bold: true,
+                                    onClick: function () {
+                                      
+                                      $this.deleteUser(userId,proyectoId);
+                                    }
+                                  },
+                                ]
+                          });
+                            
+                    }
+                },
+                {
+                    text: 'Cancelar',
+                    color: 'red'
+                },
+            ];
+            myApp.actions(buttons);
+       
+      },
+      addUserProyect: function(userId){
+            var  proyecto = $('#idProyectoUser').val();
+            var $this = this;
+            var token = window.localStorage.user_token;
+        
+            $$.ajax({
+                url     : 'http://35.211.157.80/appmanager/api/projectmember/store',
+                method  : 'POST',
+                dataType: 'json',
+                headers  : {"Authorization": "Bearer " + token,
+                            "Accept": "application/json ",
+                            "Content-Type": "application/x-www-form-urlencoded",},
+                data:{
+                      'USER_PROYECTO': proyecto,
+                      'USER_USER'    : userId
+                      },
+                success: function(response){
+                       if(response.success == true){
+                          //myApp.alert(response.data.message,'Corecto');
+                          //jProyecto.list_proyect('indexProjectList');
+                          $('#userAdd_'+userId).hide();
+                          jUser.listProyectUser('listUserProject',proyecto);
+                       }else{
+                          myApp.alert(response.data.message,'Error');
+                       }
+               
+                },
+                error: function(xhr, status){
+                
+                  //alert('Error: '+JSON.stringify(xhr));
+                  //alert('ErrorStatus: '+JSON.stringify(status));
+                }
+              });     
+
+      },
+      search_user: function(DomElement,search_inpt) {
+
+        var $this = this;
+        var token = window.localStorage.user_token;
+
+        $$.ajax({
+            url     : 'http://35.211.157.80/appmanager/api/user/index',
+            method  : 'POST',
+            dataType: 'json',
+            headers : {"Authorization": "Bearer " + token,
+                       "Accept"       : "application/json ",
+                       "Content-Type" : "application/x-www-form-urlencoded",},
+            data    :{
+                       'SEARCH'        : $('#search_user').val(),
+                     },
+            success : function(response){
+              var html = "";
+              if(response.data.users.length > 0){
+                  $.each(response.data.users, function( index, value ) {
+
+                    html+= '<li id="userAdd_'+value.id+'" class="list-group-item">';
+                    html+= '   <a href="#" class="media">';
+                    html+= '       <div class="media-body">';
+                    html+= '           <h5>'+value.name+'</h5>';
+                    html+= '           <p>'+value.alias+'</p>';
+                    html+= '       </div>';
+                    html+= '       <div class="w-auto">';  
+                    html+= '       <button id="btnAddUserProyect" onclick="jUser.addUserProyect('+value.id+');" style="margin: 20px 15px 20px 15px; font-size: 10px; line-height: 16px;" class="mb-2 btn btn-outline-primary open-prompt" data-toggle="modal" data-target="#addTableroModal">Añadir</button>';                
+                    html+= '       </div>';
+                    html+= '   </a>';
+                    html+= '</li>';
+
+                  });
+
+                  $('#'+DomElement).html(html);
+
+              }else{
+                  $('#'+DomElement).html('<p>Sin coincidencia</p>');
+              }
+             
+            },
+            error: function(xhr, status){
+              //alert('Error: '+JSON.stringify(xhr));
+              //alert('ErrorStatus: '+JSON.stringify(status));
+            }
+          });  
+        
+      },
+
+
+
+  }
+}();
---
+++ platforms/android/platform_www/cordova_plugins.js
@@ -0,0 +1,30 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+  {
+    "id": "cordova-plugin-spinner.SpinnerPlugin",
+    "file": "plugins/cordova-plugin-spinner/www/spinner-plugin.js",
+    "pluginId": "cordova-plugin-spinner",
+    "clobbers": [
+      "SpinnerPlugin"
+    ]
+  },
+  {
+    "id": "cordova-plugin-statusbar.statusbar",
+    "file": "plugins/cordova-plugin-statusbar/www/statusbar.js",
+    "pluginId": "cordova-plugin-statusbar",
+    "clobbers": [
+      "window.StatusBar"
+    ]
+  }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+  "cordova-plugin-console": "1.0.7",
+  "cordova-plugin-spinner": "1.1.0",
+  "cordova-plugin-statusbar": "1.0.1",
+  "cordova-plugin-whitelist": "1.2.2",
+  "cordova-support-google-services": "1.3.1"
+};
+// BOTTOM OF METADATA
+});
